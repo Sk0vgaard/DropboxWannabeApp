6ea985f379f96cba15b6313acb674f2b467990f5
Sk0vgaard <Sk0vgaard@live.dk>
2018-02-21 15:04:47 +0100

    Added components & routing
    TO:
    auth, home and user.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { AuthModule } from './auth/auth.module';
+import { HomeModule } from './home/home.module';
+import { UserModule } from './user/user.module';
 
 
 @NgModule({
@@ -14,6 +18,10 @@ import { AppComponent } from './app.component';
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    AppRoutingModule,
+    AuthModule,
+    HomeModule,
+    UserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
