3b7748b8f9d18c656bfc61e862d874eb627411f6
Sk0vgaard <Sk0vgaard@live.dk>
2018-02-26 11:23:35 +0100

    Scaling
    Can now scale from Large to XS.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { MediaChange, ObservableMedia } from '@angular/flex-layout';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
+
   routes = [
     {route: '/', title: 'Home', icon: 'home'},
     {route: '/login', title: 'Login', icon: 'lock_open'},
     {route: '/profile', title: 'Profile', icon: 'account_circle'},
     {route: '/files', title: 'Files', icon: 'folder'},
     ];
+
   navBarOpen = true;
+  watcher: Subscription;
+
+  constructor(media: ObservableMedia) {
+    this.watcher = media.subscribe((change: MediaChange) => {
+      if ( change.mqAlias === 'xs') {
+        this.loadMobileContent();
+      }else {
+        this.loadDashboardContent();
+      }
+    });
+  }
+
 
 
   toggleNav() {
     this.navBarOpen = !this.navBarOpen;
   }
+
+  private loadDashboardContent() {
+    console.log('Large View');
+  }
+
+  loadMobileContent() {
+    // Do something special since the viewport is currently
+    // using mobile display sizes
+    console.log('Small View');
+  }
+
+  ngOnDestroy(): void {
+    this.watcher.unsubscribe();
+  }
+
 }
