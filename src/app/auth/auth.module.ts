1b902dbb2dc08919749f196d2866bb1e3289578e
Sk0vgaard <Sk0vgaard@live.dk>
2018-03-03 16:20:42 +0100

    ALOT OF HTML & CSS

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -6,8 +6,13 @@ import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AuthService } from './shared/auth.service';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import {  FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatCardModule, MatFormFieldModule, MatInputModule, MatSnackBarModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatCardModule, MatFormFieldModule, MatInputModule, MatSnackBarModule,
+  MatTabsModule
+} from '@angular/material';
 import { AuthGaurd } from './shared/auth-gaurd.service';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
@@ -20,6 +25,9 @@ import { AuthGaurd } from './shared/auth-gaurd.service';
     MatFormFieldModule,
     MatInputModule,
     MatCardModule,
+    RouterModule,
+    MatTabsModule,
+    MatButtonModule,
   ],
   declarations: [LoginComponent, RegisterComponent],
   providers: [AuthService, AuthGaurd]
