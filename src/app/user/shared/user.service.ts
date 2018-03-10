7f439ccdfad8af55712e35b922dca19dc1dd8a8e
Sk0vgaard <Sk0vgaard@live.dk>
2018-03-10 14:50:06 +0100

    Bug fixing

--- src/app/user/shared/user.service.ts
+++ src/app/user/shared/user.service.ts
@@ -4,6 +4,7 @@ import { AuthService } from '../../auth/shared/auth.service';
 import { User } from './user';
 import { AngularFirestore } from 'angularfire2/firestore';
 import 'rxjs/add/operator/first';
+import {EmptyObservable} from 'rxjs/observable/EmptyObservable';
 
 @Injectable()
 export class UserService {
@@ -16,14 +17,22 @@ export class UserService {
     // Get the authUser, using switchMap for returning dbUser and dbUser same time
     return this.authService.getAuthUser() // first() is being used to shutdown the connection afterwards
       .switchMap(authUser => {
+        if (!authUser) {
+          // Using EmptyObservable to avoid a null reference exception. (gets a null value back).
+          return new EmptyObservable();
+        }
         return this.afs.doc<User>('users/' + authUser.uid)
           .valueChanges() // an observable that listen and grabbing data from specific user collection, then let me know
           // Merge authUser and dbUser, using Map for merging switchMap and returning it all.
           .map(dbUser => {
-            // Get the dbUser, using switchMap for returning dbUser and authUser same time.
-            dbUser.uid = authUser.uid;
-              dbUser.email = authUser.email;
-              return dbUser;
+            if (dbUser) {
+              // Get the dbUser, using switchMap for returning dbUser and authUser same time.
+              authUser.username = dbUser.username;
+              authUser.firstName = dbUser.firstName;
+              authUser.middleName = dbUser.middleName;
+              authUser.lastName = dbUser.lastName;
+            }
+           return authUser;
           });
     });
   }
